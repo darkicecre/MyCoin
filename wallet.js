ab6fb1ad0b1c550f606faeaaa469ad9901320aea
darkicecre <vophiminhhieuthukhoanghia@gmail.com>
2023-03-30 13:23:13 +0700

    generate source

---
+++ wallet.js
@@ -0,0 +1,10 @@
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+class Wallet {
+    constructor() {
+        this.keyPair = ec.genKeyPair();
+        this.address = this.keyPair.getPublic('hex');
+        this.privateKey = this.keyPair.getPrivate('hex');
+    }
+}
